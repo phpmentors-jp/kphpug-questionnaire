eee21ded4ead814308791d3393903b9ddb3b3656
KUBO Atsuhiro <kubo@iteman.jp>
2012-04-06 11:11:11 +0900

    Answering サービスのテストを追加した。

---
+++ src/KPHPUG/QuestionnaireBundle/Tests/Domain/Service/AnsweringTest.php
@@ -0,0 +1,100 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP version 5.3
+ *
+ * Copyright (c) 2012 GOTO Hidenori <hidenorigoto@gmail.com>,
+ *               2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    KPHPUGQuestionnaireBundle
+ * @copyright  2012 GOTO Hidenori <hidenorigoto@gmail.com>
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since      File available since Release 0.1.0
+ */
+
+namespace KPHPUG\QuestionnaireBundle\Tests\Domain\Service;
+
+use KPHPUG\QuestionnaireBundle\Domain\Entity\AnswerFactory;
+use KPHPUG\QuestionnaireBundle\Domain\Entity\QuestionnaireItemFactory;
+use KPHPUG\QuestionnaireBundle\Domain\Service\Answering;
+
+/**
+ * @package    KPHPUGQuestionnaireBundle
+ * @copyright  2012 GOTO Hidenori <hidenorigoto@gmail.com>
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @since      Class available since Release 0.1.0
+ */
+class AnsweringTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function アンケートに回答する()
+    {
+        $questionnaireItems = array();
+        foreach (array(1, 2) as $questionnaireItemId) {
+            $questionnaireItem = \Phake::mock('KPHPUG\QuestionnaireBundle\Domain\Entity\QuestionnaireItem');
+            \Phake::when($questionnaireItem)->getId()->thenReturn($questionnaireItemId);
+            $questionnaireItems[] = $questionnaireItem;
+        }
+
+        $answerFactory = new AnswerFactory();
+        $answer = $answerFactory->create($questionnaireItems);
+
+        $answerRepository = \Phake::mock('KPHPUG\QuestionnaireBundle\Domain\Entity\AnswerRepository');
+        $questionnaireItemRepository = \Phake::mock('KPHPUG\QuestionnaireBundle\Domain\Entity\QuestionnaireItemRepository');
+        \Phake::when($questionnaireItemRepository)->find($this->anything())
+            ->thenReturn($questionnaireItems[0])
+            ->thenReturn($questionnaireItems[1]);
+
+        $entityManager = \Phake::mock('Doctrine\ORM\EntityManager');
+        \Phake::when($entityManager)->getRepository('KPHPUG\QuestionnaireBundle\Domain\Entity\Answer')
+            ->thenReturn($answerRepository);
+        \Phake::when($entityManager)->getRepository('KPHPUG\QuestionnaireBundle\Domain\Entity\QuestionnaireItem')
+            ->thenReturn($questionnaireItemRepository);
+
+        $answering = new Answering($entityManager);
+        $answering->answer($answer);
+
+        \Phake::verify($answerRepository)->add($answer);
+        \Phake::verify($questionnaireItemRepository)->find($answer->getAnswerDetail(0)->getQuestionnaireItem()->getId());
+        \Phake::verify($questionnaireItemRepository)->find($answer->getAnswerDetail(1)->getQuestionnaireItem()->getId());
+        \Phake::verify($entityManager)->flush();
+    }
+}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: utf-8
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
